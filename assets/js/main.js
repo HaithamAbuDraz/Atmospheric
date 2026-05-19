f34bcd1cc6ee32c0cf8a78faed2a7af65580be0a
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-05-19 14:51:17 +0300

    Add app configuration and global state

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,22 @@ import {
   renderSunriseSunset, renderAirQuality, renderChart
 } from './ui-renderers.js';
 import { initParticles, resizeCanvas, updateParticles, cleanupParticles, startParticles } from './particles.js';
+
+// Configuration
+const APP_CONFIG = {
+  DEFAULT_CITY: 'Gaza',
+  UNITS: 'metric',
+  MAX_RECENT_SEARCHES: 6,
+  REFRESH_INTERVAL: 10 * 60 * 1000,
+  PARTICLE_COUNT: 60,
+};
+
+// State
+let state = {
+  units: APP_CONFIG.UNITS,
+  currentWeather: null,
+  forecast: null,
+  airPollution: null,
+  recentSearches: [],
+  weatherCondition: 'Clear',
+};
